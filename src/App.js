cf0064f6151848f3af5af2abc06549a10a976aa9
Matt Jarrett <cujarrett@gmail.com>
2020-02-09 20:01:41 -0600

    Adds initial create-react-app content

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Number of barrels of oil left in the world</p>
+        <p>1,518,391,709,631</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
