85b1a6b9fc5b6fcd80768422c40e91e215c23d79
Matt Jarrett <cujarrett@gmail.com>
2021-06-14 23:42:45 -0500

    feat: adds IaC

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,16 @@
 variable "aws_region" {
   description = "AWS region for the infrastructure"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "acm_certificate_arn" {
   description = "ACM Certificate for the domain"
-  type = string
+  type        = string
 }
 
 variable "site_name" {
   description = "Site name"
-  type = string
-  default = "oilcountdown.info"
+  type        = string
+  default     = "oilcountdown.info"
 }
